9705fc41319eaa1918eb6ed6e3004e879e38c4b1
Cryspysam <ssmishra9918@gmail.com>
2025-03-20 00:35:17 +0530

    Updated features

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,18 @@
 (() => {
     'use strict'
   
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
   
+    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-          console.log("Form validation failed"); // Debugging
-        } else {
-          console.log("Form submitted successfully"); // Debugging
+          event.preventDefault()
+          event.stopPropagation()
         }
   
-        form.classList.add('was-validated');
-      }, false);
+        form.classList.add('was-validated')
+      }, false)
     })
-})();
+  })()
