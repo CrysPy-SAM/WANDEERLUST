efeac8df934ee2f2f331b82012cfbafb7d63d6aa
Cryspysam <ssmishra9918@gmail.com>
2025-06-20 23:56:22 +0530

    Your commit message here

--- routes/review.js
+++ routes/review.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");const Review = require("../models/review.js");
-const {validateReview, isLoggedIn} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 router.post(
   "/",
@@ -27,6 +27,8 @@ router.post(
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   wrapAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
