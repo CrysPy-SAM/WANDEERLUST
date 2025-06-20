cd3c3248667155f8e5b82f0eda68fe0977f5fb31
Cryspysam <ssmishra9918@gmail.com>
2025-06-20 15:02:06 +0530

    Your commit message here

--- init/index.js
+++ init/index.js
@@ -18,6 +18,9 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data= initData.data.map((obj)=>({
+      ...obj, owner: "682051c3db4d13644ca2cbaa"
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
